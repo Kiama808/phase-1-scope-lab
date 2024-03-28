17db6931debe520e6a8bd693455456809456d938
Kelvin <kelvinmurithi2005@gmail.com>
2024-03-28 12:50:31 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 // Write your solution in this file!
 
-var customerName = 'bob'
-const leastFavoriteCustomer = 'some initial value'
+var customerName = "bob";
+const leastFavoriteCustomer = "some initial value";
 
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase()
-  
-  //notice that there is no return statement
-  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
+  customerName = customerName.toUpperCase();
 }
 
 function setBestCustomer() {
-  bestCustomer = 'not bob'
+  bestCustomer = "not bob";
 }
 
 function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob'
+  bestCustomer = "maybe bob";
 }
 
 function changeLeastFavoriteCustomer() {
-  leastFavoriteCustomer = 'billy'
+  leastFavoriteCustomer = "billy";
 }
